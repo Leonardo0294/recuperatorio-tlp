8d3c02f14f074216fd7d819cfc0fa1f5d7232237
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-07-05 14:25:23 -0300

    se creo el modelo de datos para las reservas

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,65 @@
-// TODO: Crear modelo de datos de Reserva
+const { sequelize, DataTypes } = require("../database");
+
+const Reserva = sequelize.define(
+  "reserva",
+  {
+    // Definición de las propiedades del modelo
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    fecha: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    // Opciones del modelo
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: "reservas",
+  }
+);
+
+(async () => {
+  await Reserva.sync();
+  console.log('Tabla "reservas" creada en la base de datos');
+})();
+
+module.exports = Reserva;
