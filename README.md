9e2eb740c722009dee41e2eb3cb560ae3ddf942a
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-07-05 14:21:42 -0300

    se completó el codigo faltante en el archivo de arranque app.js

---
+++ README.md
@@ -0,0 +1,32 @@
+# Pasos para instalar y ejecutar el proyecto creado para el examen
+Instalaciones necesarias:
+Dentro del directorio del proyecto ejecutar:
+```bash
+ Ejecuta `npm install` para instalar las dependencias.
+```
+
+>> ### Requiere de una base de datos MySQL.
+
+Configura las variables de entorno en el archivo `.env`.
+
+Variables de entorno:
+```bash
+PORT=
+DB_NAME=
+DB_USER=
+DB_PASSWORD=
+DB_HOST=
+DB_PORT=
+DB_DIALECT=
+SECRET_KEY=clave-secreta-de-desarrollo
+```
+
+Ejecutar el proyecto en modo desarrollo:
+```bash
+npm run dev
+```
+
+Ejecutar el proyecto en modo producción:
+```bash
+npm run start
+```
--- app.js
+++ app.js
@@ -1,20 +1,62 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const path = require('path');
+const path = require("path");
+
+require("dotenv").config();
+
+const { sequelize } = require("./database");
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Conexión a base de datos exitosa"))
+  .catch((error) => console.log("Error al conectar a base de datos", error));
 
 const app = express();
 
+require("ejs");
+
+// Configuración del motor de plantillas
+app.set("views", path.join(__dirname, "views")); // Corrección: Se corrige la sintaxis del método 'join'
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+const port = process.env.PORT || 5000;
+
 // Middlewares
 // TODO: Implementar middlewares
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+//app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
+app.use((req, res, next) => {
+  res.write(`<div>
+        <h1>404 - Ruta no encontrada</h1>
+        <hr>
+        <p>La pagina que intentas buscar no existe</p>
+        <p>Redireccionando a la página de inicio...</p>
+        <script>
+        (
+          () => setTimeout(() => {
+            window.location.href='http://localhost:${port}/';
+           }, 3000)           
+        )();
+        </script>
+    </h1>`);
+});
+
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, console.log(`Servidor corriendo en http://localhost:${port}`));
